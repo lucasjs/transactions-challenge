664d29441610c88f623a5dc4e2af20445ab3d237
Lucas J S <lucas.tcjb@gmail.com>
2019-10-20 10:58:25 -0300

    Initial commit

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,15 @@
+const custom = require('../webpack.config.js')
+
+module.exports = async ({ config, mode }) => {
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: custom.module.rules
+    },
+    resolve: {
+      ...config.resolve,
+      modules: custom.resolve.modules
+    }
+  }
+}
